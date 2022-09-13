19c10425648a52cbb9a5497580859242b2ee277c
weihaopeng <weihaopeng@mq-ai.cn>
2022-09-13 17:01:21 +0800

    #8 enhance new Style, modify Nav.

--- src/components/common/Nav.vue
+++ src/components/common/Nav.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
 ALayout
-  ALayoutHeader
-    .logo(@click="goHome") W3 POC Testnet
+  ALayoutHeader.layout-header
+    .logo(@click="goHome")
+      span W
+      span 3
+      span POC Testnet
     AMenu(
-      theme="dark"
       mode="horizontal"
       v-model:selectedKeys="selectedRoute"
-      :style="{ lineHeight: '64px', marginLeft: '16px' }"
       @click="linkToRoute"
     )
       AMenuItem(key="benchmark") Benchmark
@@ -44,7 +45,7 @@ export default defineComponent({
     }
 
     const goHome = () => {
-      router.push({ name: 'Benchmark' })
+      // router.push({ name: 'Benchmark' })
     }
 
     return {
@@ -60,28 +61,61 @@ export default defineComponent({
 <style lang="sass" scoped>
 .ant-layout
   height: 100%
+  background: #3d3f50
 
   &-header
     display: flex
     align-items: center
     line-height: unset
+    height: 90px
+    background: #ffffff
+    box-shadow: 0px 1px 8px 0px rgba(0,0,0,0.15)
+    padding: 0 30px
 
     .logo
       display: inline-block
-      color: #fff
-      font-size: 32px
-      margin-right: 48px
+      font-size: 30px
+      margin-right: 312px
       user-select: none
-      cursor: pointer
+      // cursor: pointer
+      font-weight: bolder
+      span:first-child
+        background: linear-gradient(180deg,#1acd57 7%, #0093c2 83%)
+        background-clip: text
+        -webkit-background-clip: text
+        color: transparent
+      span:last-child
+        font-weight: normal
+        margin-left: 12px
 
     .ant-menu
       flex-grow: 1
+      line-height: 90px
+      color: rgba(0, 0, 0, 0.5)
+      font-size: 20px
+      :deep .ant-menu-item
+        padding: 0
+        font-weight: 400
+        transition: all 0.3s
+        .ant-menu-title-content
+          padding: 0 60px
+        &:after
+          border-bottom-width: 8px
+          border-color: transparent
+          left: 0
+          right: 0
+        &-selected
+          color: #000
+          font-weight: normal
+          &::after
+            border-image: linear-gradient(90deg,#1acd57 3%, #0093c2 96%) 1
+        &:hover
+          color: #000
+          &:after
+            border-color: transparent
 
   &-content
-    height: calc(100% - 80px - 64px)
-    margin: 16px 50px
-    background-color: #fff
-    position: relative
+    flex-grow: 1
 
 </style>
   
