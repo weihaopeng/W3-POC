d9825d7b51a27dacda2ad8fe98e70521de3d0290
wangqing <wangqing@mq-ai.cn>
2022-09-23 18:22:13 +0800

    #20 fix the broken commit

---
+++ src/w3/core/entities/index.js
@@ -0,0 +1,8 @@
+import { Account } from './account.js'
+import { Transaction} from './transaction.js'
+import { BlockProposal } from './block-proposal.js'
+import { Block } from './block.js'
+import { Chain } from './chain.js'
+import { Fork } from './fork.js'
+
+export { Account, Transaction, BlockProposal, Block, Chain, Fork }
