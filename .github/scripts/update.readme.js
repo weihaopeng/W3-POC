647c19fa27dbfe7c79d62c5ccf69a02d88eb8670
Jianru Lin <jianru.lin@gmail.com>
2022-09-24 23:09:52 +0800

    update ci

--- .github/scripts/update.readme.js
+++ .github/scripts/update.readme.js
@@ -9,7 +9,21 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const readmePath = path.join(__dirname, '../README.md')
+const readmePath = path.join(__dirname, '../../README.md')
+const ipfsPath = process.argv[2]
+
+const readCID = () => {
+  const content = fs.readFileSync(ipfsPath, 'utf8')
+  const cidList = content
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => line && true)
+    .map((line) => {
+      const [_, id, filename] = line.split(/\s+/)
+      return id
+    })
+  return cidList[cidList.length - 1] // last one is what we want
+}
 
 const updateUrls = (CID) => {
   const readme = fs.readFileSync(readmePath, 'utf8')
@@ -21,4 +35,4 @@ const updateUrls = (CID) => {
   console.log('Updated README.md Access Urls with new CID: ', process.argv[2])
 }
 
-updateUrls(process.argv[2])
+updateUrls(readCID())
